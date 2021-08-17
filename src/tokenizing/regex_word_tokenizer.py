4b3db83662484e876a2dda0a1d2d6a20ba0c8953
Paul Tuck <pau1tuck@outlook.com>
2021-08-18 02:19:26 +0800

    "2021-08-18T02:19CST"

--- src/tokenizing/regex_word_tokenizer.py
+++ src/tokenizing/regex_word_tokenizer.py
@@ -10,4 +10,13 @@ regexp_tokenize("I can't understand you.", "[\w']+")
 tokenizer = RegexpTokenizer("[\w']+")
 tokenizer.tokenize("Can't is a contraction.")
 
-whitespace_tokenizer = RegexpTokenizer('\s+', gaps=True)
+# The gaps parameter, if set to True, is used to find the gaps between the tokens. Otherwise, it is used to find the tokens themselves.
+whitespace_tokenizer = RegexpTokenizer(r'\s+', gaps=True)
+word_tokenizer = RegexpTokenizer(pattern=r'\w+', gaps=False)
+
+sentence = "The brown fox wasn't that quick and he couldn't win the race."
+
+word_indices = list(whitespace_tokenizer.span_tokenize(sentence))
+print(word_indices)
+print([sentence[start:end] for start, end in word_indices])
+print(word_tokenizer.tokenize(sentence))
