cbcf8dcc0c2124c1919612ef46224b6c31d5aeda
Paul Tuck <pau1tuck@outlook.com>
2021-08-26 13:19:15 +0800

    "2021-08-26T13:19CST"

---
+++ src/replacing/replace_contractions.py
@@ -0,0 +1,5 @@
+from replacers import RegexpReplacer
+replacer = RegexpReplacer()
+
+print(replacer.replace("Is \"they're\" a contraction?"))
+print(replacer.replace("I should've done that thing that I didn't do."))
